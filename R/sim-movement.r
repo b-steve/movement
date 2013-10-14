fcbf8a1578142112977ffae09416c531c6d1f35c
b-steve <bcs5@st-andrews.ac.uk>
2013-10-14 19:59:17 +0100

    Fixing spelling error.

--- R/sim-movement.r
+++ R/sim-movement.r
@@ -4,16 +4,18 @@
 #'
 #' @param n Number of points to simulate.
 #' @param kappa Concentration parameter for the Von-Mises distribution.
-#' @param a Mean stop length for the gamma distribution.
+#' @param a Mean step length for the gamma distribution.
 #' @param b Step length standard deviation for the gamma distribution.
 #' @param e Measurement error standard deviation for the bivariate normal distribution.
 #' @param phi0 Initial bearing.
 
-sim.movement <- function(n, kappa, a, b, e, phi0){
+sim.movement <- function(n, kappa, a, b, e){
   library(CircStats)
-  X <- matrix(0, nrow = n, ncol = 2)
+  X <- matrix(0, nrow = n + 1, ncol = 2)
+  ## Start location is (0, 0); Original bearing is 0
   x <- c(0, 0)
-  phi <- phi0
+  X[1, ] <- x
+  phi <- 0
   for (i in 1:n){
     ## Calculate new bearing
     mu <- rvm(1, mean = pi, k = kappa) - pi
@@ -31,15 +33,17 @@ sim.movement <- function(n, kappa, a, b, e, phi0){
     x <- x + x.delta
     X[i, ] <- x
   }
-  error <- matrix(rnorm(2*n, mean = 0, sd = e), nrow = n, ncol = 2)
+  error <- matrix(rnorm(2*(n + 1), mean = 0, sd = e), nrow = n + 1, ncol = 2)
   Xobs <- X + error
   list(X = X, Xobs = Xobs)
 }
 
-locs <- sim.movement(n = 100, kappa = 3, a = 20, b = 8, e = 15, phi0 = 0)
+## Simulate data
+locs <- sim.movement(n = 100, kappa = 3, a = 20, b = 8, e = 15)
+## Plot both observed (black) and actual (blue) locations
 xlim <- range(sapply(locs, function(x) range(x[, 1])))
 ylim <- range(sapply(locs, function(x) range(x[, 2])))
-plot(locs$X, type = "n", xlim = xlim, ylim = ylim, asp = 1, col = "blue")
+plot(locs$X, type = "l", xlim = xlim, ylim = ylim, asp = 1, col = "blue")
 points(locs$X, col = "blue")
 lines(locs$Xobs)
 points(locs$Xobs)
