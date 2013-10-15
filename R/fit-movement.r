650e76e7ab833d3ead54eb18e7d3399e93cb2247
Ben Stevenson <bcs5@st-andrews.ac.uk>
2013-10-15 21:10:55 +0100

    Adding windows executable and removing compilation function.

--- R/fit-movement.r
+++ R/fit-movement.r
@@ -2,7 +2,7 @@ set.seed(5434)
 library(R2admb)
 source("sim-movement.r")
 n <- 20
-kappa <- 60
+kappa <- 3.5
 a <- 20
 b <- 15
 sigma <- 10
@@ -11,14 +11,16 @@ locs <- sim.movement(n = n, kappa = kappa, a = a, b = b, sigma = sigma)
 Xobs <- locs$Xobs[-1, ]
 write_dat("movement", list(n = n, x_obs = Xobs))
 write_pin("movement", list(kappa = kappa, a = a, b = b, sigma = sigma, x = Xobs))
-compile_admb("movement", safe = TRUE, re = TRUE, verbose = TRUE)
-run_admb("movement", verbose = TRUE, extra.args = "-noinit")
+##compile_admb("movement", safe = TRUE, re = TRUE, verbose = TRUE)
+run_admb("movement", verbose = FALSE, extra.args = "-noinit")
 fit <- read_admb("movement")
 clean_admb("movement")
 
 fit$maxgrad
 summary(fit)
 
+## Plotting location point estimates (red) with observed locations
+## (black) and actual locations (blue)
 coords <- coef(fit, type = "random")
 xs <- coords[c(TRUE, FALSE)]
 ys <- coords[c(FALSE, TRUE)]
@@ -31,14 +33,16 @@ points(locs$Xobs)
 lines(xs, ys, col = "red")
 points(xs, ys, col = "red")
 
-## > summary(fit)
-## Model file: movement 
-## Negative log-likelihood:  105.9 	 AIC:  219.9 
+
+## Model summary:
+##
+## Model file: movement
+## Negative log-likelihood:  119.7 	 AIC:  231.4
 ## Coefficients:
-##       Estimate Std. Error z value Pr(>|z|)    
-## kappa   69.090     55.415   1.247 0.212478    
-## a       20.629      2.373   8.691  < 2e-16 ***
-## b       10.462      2.730   3.833 0.000127 ***
-## sigma   10.163      1.619   6.276 3.46e-10 ***
+##       Estimate Std. Error z value Pr(>|z|)
+## kappa    6.882      3.979   1.730   0.0837 .
+## a       17.698      2.595   6.819 9.16e-12 ***
+## b       11.051      2.642   4.183 2.88e-05 ***
+## sigma   11.532      1.839   6.272 3.57e-10 ***
 ## ---
 ## Signif. codes:  0 '***' 0.001 '**' 0.01 '*' 0.05 '.' 0.1 ' ' 1
