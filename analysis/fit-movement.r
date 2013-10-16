7586ef125c04cf9419e18267c971908c4a843a83
Ben Stevenson <bcs5@st-andrews.ac.uk>
2013-10-16 15:30:32 +0100

    Changing parameter values.

--- analysis/fit-movement.r
+++ analysis/fit-movement.r
@@ -1,12 +1,12 @@
-source("../R/movement-funs.r")
+source("~/movement/R/movement-funs.r")
 ## True parameter values
 n <- 200
 kappa <- 6
 a <- 20
 b <- 15
-sigma <- 15
+sigma <- 30
 ## Simulate the data
-set.seed(5143)
+set.seed(5153)
 locs <- sim.movement(n = n, kappa = kappa, a = a, b = b, sigma = sigma)
 ## Pull out observed and actual locations (first row is always (0, 0),
 ## so removing it)
@@ -15,7 +15,10 @@ X <- locs$X[-1, ]
 ## Set up list of starting parameters
 sv <- list(kappa = kappa, a = a, b = b, sigma = sigma)
 ## Fit the model
-fit <- fit.movement(Xobs, sv, dir = "../admb")
+## dir must point to the admb directory
+fit <- fit.movement(Xobs, sv, dir = "~/movement/admb")
 summary(fit)
 ## Plot estimated locations
+pdf(file = "locs.pdf")
 plot.movement(fit, X = X)
+dev.off()
